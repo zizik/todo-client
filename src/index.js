6ee4ccf1ce7fea05389b315218aebe03b04499a7
Zizik <zizidukabrik@gmail.com>
2018-05-30 08:21:24 +0300

    Move store creation to separate folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import createSagaMiddleware from "redux-saga";
 
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import reducers from "./reducers";
-import sagas from "./sagas";
-
-const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducers, applyMiddleware(sagaMiddleware));
-sagaMiddleware.run(sagas);
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
